ea5588ed85199650ee7bcb0fa33ea56c8aa9b4e1
tbtnn <82361174+tbtnn@users.noreply.github.com>
2021-05-20 16:10:24 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".faq-list-item").click(function(){
+  var $answer=$(this).find(".answer")
+   
+  if($(".answer").hasClass("open")){
+    $answer.removeClass("open");
+    $answer.slideUp();
+    $(this).children("span").text("+");
+  }else{
+    $answer.addClass("open");
+    $answer.slideDown();
+    $(this).children("span").text("-");
+  }
+  
+ 
+  
+});
